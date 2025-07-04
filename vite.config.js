40a792ac9dde4feb00f7d7ec98659c650e9b73c2
Priyank lal <priyanklal@Priyanks-MacBook-Air.local>
2025-07-04 11:05:39 +0530

    Fixed Error Message Issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from "fs";
 
-// https://vite.dev/config/
+// Increase memory limit
+process.env.NODE_OPTIONS = "--max-old-space-size=4096";
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          mui: ["@mui/material", "@mui/icons-material"],
+        },
+      },
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+  },
 });
