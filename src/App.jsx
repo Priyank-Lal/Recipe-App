4832a93ea5eddc3554499e02b4b89437e2ced483
Priyank lal <priyanklal@Priyanks-MacBook-Air.local>
2025-07-08 08:08:02 +0530

    Initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import MainRoutes from "./routes/MainRoutes";
 import "./index.css";
 import Navigation from "./components/Navigation";
-import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
     <div className="w-screen h-screen">
       <Navigation />
       <MainRoutes />
-      <Analytics />
     </div>
   );
 }
