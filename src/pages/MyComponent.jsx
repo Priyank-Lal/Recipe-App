8cfc10546256f3f5ffd849d9351a2901e7dbcd0c
Priyank lal <ratherdrag@gmail.com>
2025-09-09 10:56:39 +0530

    No comment

--- src/pages/MyComponent.jsx
+++ src/pages/MyComponent.jsx
@@ -4,7 +4,7 @@ const MyComponent = () => {
   console.log("Rendered");
   return <div>My Component,Your Component</div>;
 };
-
+// New Comment
 // Doesn't re-render if parent re-renders, unless nothing inside it changes
 export default memo(MyComponent);
 
