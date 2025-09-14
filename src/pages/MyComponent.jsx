e6329c80036cef7e2f1d554d83a387ae6d82ae94
Priyank lal <ratherdrag@gmail.com>
2025-09-14 08:50:19 +0530

    No commit

--- src/pages/MyComponent.jsx
+++ src/pages/MyComponent.jsx
@@ -4,10 +4,11 @@ const MyComponent = () => {
   console.log("Rendered");
   return <div>My Component,Your Component</div>;
 };
-// New Comment
+// New Comment-1
 // New Comment-2
 // New Commnet-3
 // New Commnet-4
+// New Commnet-5
 // Doesn't re-render if parent re-renders, unless nothing inside it changes
 export default memo(MyComponent);
 
